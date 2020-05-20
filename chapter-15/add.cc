6ee19c0fe45d0cb529d09a94ad2ca73db5c83b8f
carrieyin <626659321@qq.com>
2020-05-20 19:46:54 +0800

    管道

---
+++ chapter-15/add.cc
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+using namespace std;
+#define MAXLINE 4096
+
+int main()
+{
+    int n, int1, int2;
+    char line[MAXLINE];
+   
+    /*char s[] = "12 34";
+    int a, b;
+    sscanf(s, "%d%d", &a, &b);
+    printf("%d---%d\n", a, b);*/
+    string s;
+    while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0)
+    {
+        line[n] = 0;
+        printf("n is %d \n",n);
+        //char line[6] = "12 34";
+        printf("string is %s", string(line).c_str());
+        int temp = sscanf(line, "%d%d", &int1, &int2);
+        //printf("%d\n", temp);
+        if(temp == 2)
+        {
+            sprintf(line, "%d", int1 + int2);
+            n = strlen(line);
+            if(write(STDOUT_FILENO, line, n) != n)
+            {
+
+            }
+        }
+        else
+        {
+            if(write(STDOUT_FILENO, "invalid args\n", 13) != 13)
+            {
+
+            }
+        }
+        
+    }
+
+    exit(0);
+    
+}
